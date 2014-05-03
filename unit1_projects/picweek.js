b36bd344229b5dbd9ec577f9480e809cf8246244
Tony Leung <tleung999@gmail.com>
2014-05-02 20:17:04 -0700

    completed Challenge 4

---
+++ unit1_projects/picweek.js
@@ -0,0 +1,6 @@
+document.write('\
+			<img src="images/P1013651.jpg" width="100%"/>\
+			<caption>Picture of the week\
+			</caption>\
+			<p><br>This picture was taken when I was in the Carribean at Barbados.   A catamaran was out feeding the turtles and I had my underwater camera with me.   It was amazing seeing them swim, one even swam at me head on and bumped me in the chest.</p>\
+			');
