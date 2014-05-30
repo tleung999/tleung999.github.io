09774763e3c136126483a27739914bfba969f358
Tony Leung <tleung999@gmail.com>
2014-05-30 11:34:33 -0700

    week6 cultural and technical blog complete

--- unit1_projects/picweek.js
+++ unit1_projects/picweek.js
@@ -1,5 +1,5 @@
 document.write('\
-			<img src="images/p1011977.jpg" width="100%"/>\
+			<img src="images/P1011977.jpg" width="100%"/>\
 			<caption>Picture of the week\
 			</caption>\
 			<p><br>MotoGP in Monterey Laguna Seca 2006. One of my favorite turns to watch, the Corkscrew.</p>\
